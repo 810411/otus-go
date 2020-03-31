607c6f9f9fb47d18a86b18e6fa136f16f9c98c06
Grigorii Balabanov <810411@mail.ru>
2020-03-31 17:52:00 +0500

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,37 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
+import (
+	"sort"
+	"strings"
+	"unicode"
+)
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+func Top10(s string) []string {
+	const top10 = 10
+	var ks []string
+	cache := map[string]int{}
+
+	f := func(c rune) bool {
+		return !unicode.IsLetter(c) && !unicode.IsNumber(c) && unicode.IsSymbol('-')
+	}
+
+	for _, w := range strings.FieldsFunc(s, f) {
+		w = strings.ToLower(w)
+		cache[w]++
+	}
+
+	for k, v := range cache {
+		if v > 1 {
+			ks = append(ks, k)
+		}
+	}
+
+	sort.Slice(ks, func(i, j int) bool {
+		return cache[ks[i]] > cache[ks[j]]
+	})
+
+	if len(ks) > top10 {
+		return ks[:top10]
+	}
+
+	return ks
 }
