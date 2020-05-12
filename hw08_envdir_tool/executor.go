d001c88adabc8279c3132bbe00009f0bfff42a60
Grigorii Balabanov <810411@mail.ru>
2020-05-12 12:25:58 +0500

    HW8 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,32 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here
-	return
+	name, args := cmd[0], cmd[1:]
+
+	proc := exec.Command(name, args...)
+	proc.Stderr = os.Stderr
+	proc.Stdout = os.Stdout
+	proc.Stdin = os.Stdin
+
+	if env != nil {
+		envs := make([]string, 0, len(env))
+		for k, v := range env {
+			envs = append(envs, k+"="+v)
+		}
+
+		proc.Env = envs
+	}
+
+	if err := proc.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	return 0
 }
